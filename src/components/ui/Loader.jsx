f81156d5d2db78605119d8cfe23dddf22eb7a1a2
govind-codes-byte <gs1036907@gmail.com>
2026-06-14 12:49:16 +0530

    loader

--- src/components/ui/Loader.jsx
+++ src/components/ui/Loader.jsx
@@ -45,7 +45,7 @@ const Loader = () => {
             Initializing
           </motion.p>
           <motion.div className="flex gap-1.5 justify-center">
-            {['D', 'E', 'V', '.', 'P', 'O', 'R', 'T', 'F', 'O', 'L', 'I', 'O'].map((char, i) => (
+            {['G', 'O', 'V','I','N','D','.', 'P', 'O', 'R', 'T', 'F', 'O', 'L', 'I', 'O'].map((char, i) => (
               <motion.span
                 key={i}
                 className="font-display font-bold text-lg text-white/90"
